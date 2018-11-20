d26f08d8aa48f200c6f93add091b624d7360a5fc
Dima Kurilo <dkurilo@gmail.com>
2018-11-20 14:25:19 -0500

    small fixes in deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,4 +2,4 @@
 docker-compose up
 ssh dima@battleship-fp.com 'rm -rf /var/battleship/site'
 scp -r ./out dima@battleship-fp.com:/var/battleship/site
-ssh -t dima@battleship-fp.com 'chown -R dima:battleship /var/battleship; chmod -R 750 /var/battleship; chmod -R 770 /var/battleship/log; cp -r /var/battleship/local/* /var/battleship/site/public/; cd /var/battleship; sudo setcap CAP_NET_BIND_SERVICE=+eip /var/battleship/site/battleship; sudo service supervisor restart'
+ssh -t dima@battleship-fp.com 'cp -r /var/battleship/local/* /var/battleship/site/public/; chown -R dima:battleship /var/battleship; chmod -R 750 /var/battleship; chmod -R 770 /var/battleship/log; cd /var/battleship; sudo setcap CAP_NET_BIND_SERVICE=+eip /var/battleship/site/battleship; sudo service supervisor restart'
