86d845f696931d272abcc735582b49b6c8708763
Dima Kurilo <dkurilo@gmail.com>
2020-01-28 22:08:39 -0500

    fix errors

--- deploy/build.sh
+++ deploy/build.sh
@@ -10,7 +10,9 @@ cp  -r ./build ../deploy/out/public
 
 cd ..
 
-stack install --local-bin-path ../deploy/out/
+stack install --local-bin-path ./deploy/out/
 
-cp ./rules.json ../deploy/out/
-cp ./rules.json ../deploy/out/rules.bot.json
+cp ./server/rules.json ./deploy/out/
+cp ./bot/rules.json ./deploy/out/rules.bot.json
+
+cd ./deploy
