25fed3a7fe752d31fa47670f4b667306ccc5f085
Dima Kurilo <dkurilo@gmail.com>
2020-01-30 11:21:50 -0500

    fix deploy scripts, package and travis config to make possible CI/CD as next step

--- deploy/build.sh
+++ deploy/build.sh
@@ -3,9 +3,12 @@ cd /var/build/
 rm -rf ./deploy/out
 mkdir -p ./deploy/out
 
+export CI=true
+export NODE_ENV=production
+
 cd /var/build/client
 yarn install --force
-yarn build:prod
+yarn build --frozen-lockfile
 cp  -r ./build ../deploy/out/public
 
 cd ..
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 docker-compose up
 ssh dima@battleship-fp.com 'rm -rf /var/battleship/site'
+find ./out -type f -name ".DS_Store" -exec rm {} \;
 scp -r ./out dima@battleship-fp.com:/var/battleship/site
-ssh -t dima@battleship-fp.com 'cp -r /var/battleship/local/* /var/battleship/site/public/; chown -R dima:battleship /var/battleship; chmod -R 750 /var/battleship; chmod -R 770 /var/battleship/log; cd /var/battleship; sudo setcap CAP_NET_BIND_SERVICE=+eip /var/battleship/site/battleship; sudo service supervisor restart'
+ssh -t dima@battleship-fp.com 'cp -r /var/battleship/local/* /var/battleship/site/public/; chown -R dima:battleship /var/battleship/site; chmod -R 750 /var/battleship/site; cd /var/battleship; sudo setcap CAP_NET_BIND_SERVICE=+eip /var/battleship/site/battleship; sudo service supervisor restart'
